e5fd30e6e9037d42cb5759d8642e6fe4e390224f
adrien <adrien.constan@gmail.com>
2021-11-11 12:14:00 +0100

    auto-commit

--- includes/stack.hpp
+++ includes/stack.hpp
@@ -5,57 +5,8 @@
 
 namespace ft
 {
-	template <typename T, typename Container = ft::vector<T> >
-		class stack
-		{
-			public:
-				typedef	T					value_type;
-				typedef	Container			container_type;
-				typedef	std::size_t		size_type;
-
-				container_type		_ctnr;
-
-				explicit stack (const container_type& ctnr = container_type()) : _ctnr(ctnr) {};
-				virtual ~stack() {};
-
-				bool empty() const
-				{
-					return (_ctnr.is_empty());
-				}
-
-				size_type size() const
-				{
-					return (_ctnr.size());
-				}
-
-				value_type &top()
-				{
-					return (_ctnr.back());
-				}
-
-				const value_type &top() const
-				{
-					return (_ctnr.back());
-				}
-
-				void push(const value_type &val)
-				{
-					_ctnr.push_back(val);
-				}
-
-				void pop()
-				{
-					_ctnr.pop_back();
-				}
-
-				friend bool operator== <T, Container>(stack const& lhs, stack const& rhs);
-				friend bool operator!= <T, Container>(stack const& lhs, stack const& rhs);
-				friend bool operator< <T, Container>(stack const& lhs, stack const& rhs);
-				friend bool operator<= <T, Container>(stack const& lhs, stack const& rhs);
-				friend bool operator> <T, Container>(stack const& lhs, stack const& rhs);
-				friend bool operator>= <T, Container>(stack const& lhs, stack const& rhs);
-
-		};
+	template <class T, class Container = vector<T> >
+		class stack;
 	template <class T, class Container>
 		bool operator==(stack<T,Container> const& lhs, stack<T,Container> const& rhs)
 		{
@@ -91,5 +42,55 @@ namespace ft
 		{
 			return lhs._ctnr >= rhs._ctnr;
 		}
+
+	template <typename T, typename Container>
+		class stack
+		{
+			public:
+				typedef	T					value_type;
+				typedef	Container			container_type;
+				typedef	std::size_t		size_type;
+
+				container_type		_ctnr;
+
+				explicit stack (const container_type& ctnr = container_type()) : _ctnr(ctnr) {};
+				virtual ~stack() {};
+
+				bool empty() const
+				{
+					return (_ctnr.empty());
+				}
+
+				size_type size() const
+				{
+					return (_ctnr.size());
+				}
+
+				value_type &top()
+				{
+					return (_ctnr.back());
+				}
+
+				const value_type &top() const
+				{
+					return (_ctnr.back());
+				}
+
+				void push(const value_type &val)
+				{
+					_ctnr.push_back(val);
+				}
+
+				void pop()
+				{
+					_ctnr.pop_back();
+				}
+
+				friend bool operator== <T, Container>(stack const& lhs, stack const& rhs);
+				friend bool operator!= <T, Container>(stack const& lhs, stack const& rhs);
+				friend bool operator< <T, Container>(stack const& lhs, stack const& rhs);
+				friend bool operator<= <T, Container>(stack const& lhs, stack const& rhs);
+				friend bool operator> <T, Container>(stack const& lhs, stack const& rhs);
+				friend bool operator>= <T, Container>(stack const& lhs, stack const& rhs);};
 };
 #endif
