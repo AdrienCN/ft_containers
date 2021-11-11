56d5154162c75aa6d40c9cec8a06bb13f741d81f
adrien <adrien.constan@gmail.com>
2021-11-11 19:20:24 +0100

    auto-commit

--- includes/iterator_map.hpp
+++ includes/iterator_map.hpp
@@ -19,10 +19,9 @@ namespace ft
 			//Constructors
 			iterator_map () : _ptr(NULL) {};
 
-			iterator_map (ptr *ptr) : _ptr(ptr) {};
+			iterator_map (node *ptr) : _ptr(ptr) {};
 			iterator_map (const iterator_map &src) : _ptr(src._ptr) {};
-
-			iterator_map & operator=(const iterator_map<T> & src)
+			iterator_map & operator=(const iterator_map & src)
 			{
 				if (*this == src)
 					return *this;
@@ -52,7 +51,7 @@ namespace ft
 				return (&(_ptr->_pr))
 			}
 
-			iterator_map	operator++()
+			iterator_map & operator++()
 			{
 				node	*current = _ptr;
 				//Trouve la plus petite valeur de ma branche droite
@@ -81,7 +80,7 @@ namespace ft
 				return tmp;
 			}
 
-			iterator_map	operator--()
+			iterator_map & operator--()
 			{
 				if (_ptr->left)
 					_ptr = findMaxChild(ptr->left);
@@ -108,6 +107,109 @@ namespace ft
 			//end of iterator_map
 	};
 
+	template <typename T>
+		class const_iterator_map : public ft::iterator<ft::bidirectional_tag, T>
+	{
+		public:
+			typedef std::ptrdiff_t					difference_type;
+			typedef const T							value_type;
+			typedef const T&						reference;
+			typedef const T*						pointer;
+			typedef ft::bidirectional_tag	iterator_category;
+			typedef node<T>					node;
+
+			//Attribut
+			node		*_ptr;
+			//Constructors
+			const_iterator_map () : _ptr(NULL) {};
+			const_iterator_map (node *ptr) : _ptr(ptr) {};
+			const_iterator_map (const const_iterator_map &src) : _ptr(src._ptr) {};
+			const_iterator_map (const const_iterator_map<T> &src) : _ptr(src._ptr) {};
+			const_iterator_map & operator=(const const_iterator_map & src)
+			{
+				if (*this == src)
+					return *this;
+				_ptr = src._ptr;
+				return *this;
+			}
+
+			virtual ~const_iterator_map() {};
+
+			bool operator==(const const_iterator_map & rhs)
+			{
+				return (_ptr == rhs)
+			}
+
+			bool operator!=(const const_iterator_map & rhs)
+			{
+				return (_ptr != rhs)
+			}
+
+			reference operator*() const
+			{
+				return (_ptr->_pr);
+			}
+
+			pointer operator->() const
+			{
+				return (&(_ptr->_pr))
+			}
+
+			const_iterator_map & operator++()
+			{
+				node	*current = _ptr;
+				//Trouve la plus petite valeur de ma branche droite
+				if (current->right)
+					_ptr = findMinChild(current->right);
+				//Trouve mon premier parent > moi
+				else
+				{
+					node	*parent = _ptr->parent;
+					//Remonter l'arbre:
+					//Tant que j'ai un parent ET que je suis son fils droit
+					while (parent && parent->right == current)
+					{
+						current = parent;
+						parent = parent->parent;
+					}
+					_ptr = parent;
+				}
+				return (*this);
+			}
+
+			const_iterator_map	operator++(int)
+			{
+				const_iterator_map tmp(*this);
+				++(*this);
+				return tmp;
+			}
+
+			const_iterator_map & operator--()
+			{
+				if (_ptr->left)
+					_ptr = findMaxChild(ptr->left);
+				else
+				{
+					node *parent = _ptr->parent;
+					node *current = _ptr;
+					while (parent && parent->left == current)
+					{
+						current = parent;
+						parent = parent->parent;
+					}
+					_ptr = parent;
+				}
+				return (*this);
+			}
+
+			const_iterator_map	operator--(int)
+			{
+				const_iterator_map tmp(*this);
+				--(*this);
+				return tmp;
+			}
+			//end of const_iterator_map
+	};
 	//end of namespace
 };
 #endif
