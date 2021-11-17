693d267f937443da60efd9d14da0b716fdf95578
adrien <adrien.constan@gmail.com>
2021-11-17 17:20:51 +0100

    auto-commit

--- includes/map.hpp
+++ includes/map.hpp
@@ -50,6 +50,7 @@ namespace ft
 						bool operator()(const value_type &x, const value_type &y) const
 						{
 							//std::less()(Key x, Key y)
+							//true --> left < right 
 							return _comp(x.first, y.first);
 						}
 				};
@@ -94,16 +95,16 @@ namespace ft
 					map (InputIterator first, InputIterator last,
 							const key_compare& comp = key_compare(),
 							const allocator_type& alloc = allocator_type()) :_root(NULL), _comp(comp), _allocator(alloc), _size(0)
-					{
-						for (InputIterator it(first) ; it != last; it++)
-							this->insert(*it, _root);
-					}
+			{
+				for (InputIterator it(first) ; it != last; it++)
+					this->insert(*it, _root);
+			}
 
 				map (const map& x) : _root(NULL), _comp(x._comp), _allocator(x._allocator), _size(0)
-				{
-					for (iterator it = x.begin(); it != x.end(); it++)
-						this->insert(*it, _root);
-				}
+			{
+				for (iterator it = x.begin(); it != x.end(); it++)
+					this->insert(*it, _root);
+			}
 
 				map & operator=(const map& x)
 				{
@@ -198,62 +199,160 @@ namespace ft
 					}
 				}
 
-					//Modifiers
-					void insert(const iterator it, node *node) 
-					{
-						(void)it;
-						(void)node;
+				//Modifiers
+				pair<iterator, bool>	insert(const value_type &val) 
+				{
+					node *new_node = _find_node_by_key(val.first);
+					
+					//Noeud absent. On l'insere
+					if (new_node == NULL)
+					{	
+						//vrai code recursif d'insertion
+						new_node = this->my_insert(_root, val);
+						iterator it(new_node);
+						return (ft::make_pair<iterator, bool> (it, true));
 					}
-					void insert(node *node) 
+					//Noeud deja present
+					else
 					{
-						(void)node;
+						iterator it(new_node);
+						return (ft::make_pair<iterator, bool> (it, false));
 					}
+				}
+
+
+				}
+
+				iterator insert(const iterator it, const value_type &val) 
+				{
+					return (this->insert(val).first);
+				}
+			
+
+				template <class InputIterator>
+					void insert (InputIterator first, InputIterator last)
+					{
+						while (first != last)
+						{
+							this->insert(*first);
+							first++;
+						}
+					}
+
 				/*	erase
 					swap
-				*/	void clear() {}
+					*/	void clear() {}
 
 				/*	//Observer
 					key_comp
 					value_comp
 
 
-					//Operation
-					find
-					count
-					lower_bound
-					upper_bound
-					equal_range
+				//Operation
+				find
+				count
+				lower_bound
+				upper_bound
+				equal_range
 
-					//Allocator
+				//Allocator
 
 
-					//autes
+				//autes
 				*/	void swap(const value_type & x)
-					{
-						(void)x;
-					}
+				{
+					(void)x;
+				}
 
 			private :
-					//Mes fonctions utils perso
-					node*	_find_node_by_key() 
-					{
-						return (NULL);
-					}
-					node*	_new_node()
-					{
-						return (NULL);
-					}
-					node*	_new_node(const value_type & x)
-					{
-						(void)x;
-						return (NULL);
-					}
-					void	_free_node(node *subtree) 
-					{
-						(void)subtree;
-					}
+	
 
-					//fin de class map
+				node* my_insert(node *node, const value_type &val)
+				{
+					typedef val.first		new_key;
+					typedef node->pr.first	cur_key;
+				
+
+					if (node == NULL)
+						return (_newNode(val));
+
+					// new_key > cur_key. Insertion droite
+					if (_comp(cur_key, new_key) == true)
+						this->my_insert(node->right, val);
+					// new_key < cur_key .  Insertion gauche
+					else if (_comp(cur_key, new_key) == true)
+						this->my_insert(node->left, val);
+					//Key == cur_key. Deja present . Pas d'insertion
+					else
+						return (node);
+
+					//calculer la nouvelle height
+					node->height = 
+					//balancer l'arbre apres l'insertion
+					bool balance = is_balanced(node);
+
+					if (balance == true)
+						return node;
+
+
+
+
+				}
+				node* right_rotate(node *y)
+				{
+					node	*x = y->left;
+					node	*x_right = x->right;
+
+					x->right = y;
+					y->left = x_right;
+					return (x);
+				}
+
+				node* left_rotate(node *y)
+				{
+					node	*x = y->right;
+					node	*x_left = x->left;
+
+					x->left = y;
+					y->right = x_left;
+					return (x);
+				}
+
+				void	balance_tree(node *node)
+				{
+					bool balance_ret = is_balance(node)
+						if (node == root && balance_ret == true)
+							return;
+					if (balance_ret == false)
+					{
+						int rot_case = find_case(node);
+						ft_do_case(rot_case, node);
+					}
+					if (node == root)
+						return;
+					balance_tree(node->parent);
+				}
+
+				//Mes fonctions utils perso
+				node*	_find_node_by_key() 
+				{
+					return (NULL);
+				}
+				node*	_newNode()
+				{
+					return (NULL);
+				}
+				node*	_newNode(const value_type & x)
+				{
+					(void)x;
+					return (NULL);
+				}
+				void	_free_node(node *subtree) 
+				{
+					(void)subtree;
+				}
+
+				//fin de class map
 		};
 	//end of ft namespace
 };
